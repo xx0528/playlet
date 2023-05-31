a0c11c3b87d955c27feeab860e892a2d0ef615be
xx <xx@gmail.com>
2023-05-31 12:36:33 +0800

    add:icon

---
+++ client/app/src/main/java/com/android/player/pager/fragment/MineFragment.java
@@ -0,0 +1,49 @@
+package com.android.player.pager.fragment;
+
+import com.android.iplayer.video.cache.VideoCache;
+import com.android.player.R;
+import com.android.player.base.BaseFragment;
+import com.android.player.base.BasePresenter;
+import com.android.player.utils.Logger;
+
+public class MineFragment extends BaseFragment {
+    @Override
+    protected int getLayoutID() {
+        return R.layout.fragment_mine;
+    }
+
+    @Override
+    protected void initViews() {
+    }
+
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void onInvisible() {
+        super.onInvisible();
+    }
+
+    @Override
+    protected void onVisible() {
+        super.onVisible();
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ client/app/src/main/java/com/android/player/pager/fragment/ParkFragment.java
@@ -0,0 +1,48 @@
+package com.android.player.pager.fragment;
+
+import com.android.player.R;
+import com.android.player.base.BaseFragment;
+import com.android.player.base.BasePresenter;
+
+public class ParkFragment extends BaseFragment {
+
+    @Override
+    protected int getLayoutID() {
+        return R.layout.fragment_park;
+    }
+
+    @Override
+    protected void initViews() {
+    }
+
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void onInvisible() {
+        super.onInvisible();
+    }
+
+    @Override
+    protected void onVisible() {
+        super.onVisible();
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
