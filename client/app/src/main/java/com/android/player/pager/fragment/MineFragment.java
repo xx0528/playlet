fd951d0c388d6650fa173fcd9ab64d16e1078ee3
minigame <minigame6668@gmail.com>
2023-06-01 23:27:26 +0800

    fix:mine

--- client/app/src/main/java/com/android/player/pager/fragment/MineFragment.java
+++ client/app/src/main/java/com/android/player/pager/fragment/MineFragment.java
@@ -1,6 +1,9 @@
 package com.android.player.pager.fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.android.iplayer.video.cache.VideoCache;
@@ -10,6 +13,11 @@ import com.android.player.base.BasePresenter;
 import com.android.player.utils.Logger;
 
 public class MineFragment extends BaseFragment  {
+
+    private View mViewSetting;
+    private View mViewContact;
+    private View mViewFeedback;
+    private View mViewAbout;
     @Override
     protected int getLayoutID() {
         return R.layout.fragment_mine;
@@ -17,36 +25,46 @@ public class MineFragment extends BaseFragment  {
 
     @Override
     protected void initViews() {
+
         View settingView = findViewById(R.id.mine_setting);
-        settingView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), "点击了设置", Toast.LENGTH_SHORT).show();
-            }
-        });
-        View contactView = findViewById(R.id.mine_contact);
-        contactView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), "点击了联系我们", Toast.LENGTH_SHORT).show();
-            }
-        });
-        View feedbackView = findViewById(R.id.mine_feddback);
-        feedbackView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), "点击了反馈", Toast.LENGTH_SHORT).show();
-            }
-        });
-        View aboutView = findViewById(R.id.mine_about);
-        aboutView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), "点击了关于", Toast.LENGTH_SHORT).show();
-            }
-        });
+
+        mViewSetting = findViewById(R.id.mine_setting);
+        mViewContact = findViewById(R.id.mine_contact);
+        mViewFeedback = findViewById(R.id.mine_feddback);
+        mViewAbout = findViewById(R.id.mine_about);
+        setListener();
     }
 
+    private void setListener() {
+        Onclick click = new Onclick();
+        mViewSetting.setOnClickListener(click);
+        mViewContact.setOnClickListener(click);
+        mViewFeedback.setOnClickListener(click);
+        mViewAbout.setOnClickListener(click);
+    }
+
+    private class Onclick implements View.OnClickListener {
+
+        @SuppressLint("NonConstantResourceId")
+        @Override
+        public void onClick(View view) {
+            Intent intent = null;
+            switch (view.getId()) {
+                case R.id.mine_setting:
+                    Toast.makeText(getContext(), "点击了设置", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.mine_contact:
+                    Toast.makeText(getContext(), "点击了联系我们", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.mine_feddback:
+                    Toast.makeText(getContext(), "点击了反馈", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.mine_about:
+                    Toast.makeText(getContext(), "点击了关于", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    }
     @Override
     protected BasePresenter createPresenter() {
         return null;
